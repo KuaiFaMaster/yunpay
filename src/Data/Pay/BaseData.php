6fc82bf8a0000b548db5ce40c040e67422a460a5
ccwawamiya <ccwawamiya@gmail.com>
2025-09-28 17:37:07 +0800

    实时付款接口增加字段

--- src/Data/Pay/BaseData.php
+++ src/Data/Pay/BaseData.php
@@ -48,5 +48,30 @@ class BaseData extends Base
      */
     public $notify_url = '';
 
+
+    /**
+     * 用户手机号
+     * @var
+     */
+    public $phone_no = '';
+
+    /**
+     * 互联网平台名称,填写收入来源的互联网平台名称
+     * @var
+     */
+    public $dealer_platform_name = '';
+
+    /**
+     * 用户名称/昵称,填写从业人员在收入来源的互联网平台内展示的“名称”或“昵称”全称
+     * @var
+     */
+    public $dealer_user_nickname = '';
+
+    /**
+     * 用户唯一标识码,填写从业人员在本平台内登记的具有唯一性、长期性、可追溯性的身份标识证明
+     * @var
+     */
+    public $dealer_user_id = '';
+
     protected $method = 'post';
 }
